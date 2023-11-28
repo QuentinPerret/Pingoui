15c4e8a7bc40604fb77cb6b091027b0791bbb12f
Elea Roche <elroche@ensc.fr>
2023-11-28 18:03:30 +0000

    add display

--- repartition.pl
+++ repartition.pl
@@ -25,4 +25,26 @@ create_all_groups(AllGroups , N) :-
     group_pinguin(MaleGroups,N,male),
     append(FemaleGroups,MaleGroups,AllGroups).
 
+
+display_groups([]).
+display_groups([Group | RestGroups]) :-
+    nl,
+    write('Group: '), write(Group), nl,
+    display_pinguins(Group),
+    display_groups(RestGroups).
+
+display_pinguins([]).
+display_pinguins([P | RestPinguins]) :-
+    pinguin(P, Num, Nom, Taille, Poids, Sexe, Niveau, Position),
+    write('  Pinguin '), write(Num), write(': '),
+    write('Nom: '), write(Nom), write(', '),
+    write('Taille: '), write(Taille), write(', '),
+    write('Poids: '), write(Poids), write(', '),
+    write('Sexe: '), write(Sexe), write(', '),
+    write('Niveau: '), write(Niveau), write(', '),
+    write('Position: '), write(Position), nl,
+    display_pinguins(RestPinguins).
+
+% create_all_groups(AllGroups, 5), display_groups(AllGroups).
+
 % create_all_groups(AllGroups,5).
