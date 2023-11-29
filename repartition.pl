e52a48e395b3ffe83ff329a882e444c164b198f9
Elea Roche <elroche@ensc.fr>
2023-11-29 16:57:20 +0000

    repatition imc

--- repartition.pl
+++ repartition.pl
@@ -2,50 +2,74 @@
 ?-  write('\nLoading pinguin.pl file ...\n'),
         consult('pinguin.pl'),
         write('Loading Done !\n').
-        
-% IMC calculation
-calculate_imc(Weigh, Height, IMC) :-
-    IMC is Weigh / (Height * Height).
 
-group_pinguin(Pinguins,N,Sexe) :- 
-    findall(P, pinguin(P,_,_,_,Sexe,_,_), ListPinguins),
-    random_permutation(ListPinguins, RandomList),
-    group(RandomList,N,Pinguins,Sexe).
+% Calcul de l'IMC
+calculate_imc(Poids, Taille, IMC) :-
+    IMC is Poids / (Taille * Taille).
 
-group([],_,[],_).
-group(List , N,  [Group | Groups], Sexe) :-
-    length(Group , N),
-    append(Group , Rest , List),
-    group(Rest , N , Groups , Sexe).
-group(List , N , [List] , _) :-
-    length(List,LengthList),
+% Groupe générique
+group([], _, [], _).
+group(List, N, [Group | Groups], Sexe) :-
+    length(Group, N),
+    append(Group, Rest, List),
+    group(Rest, N, Groups, Sexe).
+group(List, N, [List], _) :-
+    length(List, LengthList),
     LengthList > 0,
     LengthList < N.
 
-create_all_groups(AllGroups , N) :-
-    group_pinguin(FemaleGroups,N,female),
-    group_pinguin(MaleGroups,N,male),
-    append(FemaleGroups,MaleGroups,AllGroups).
+% Groupe par IMC
+group_pinguin_imc(Pinguins, N, Sexe, IMCRange1, IMCRange2) :-
+    findall(P, (
+        pinguin(P, _, Taille, Poids, Sexe, _, _),
+        calculate_imc(Poids, Taille, IMC),
+        IMC >= IMCRange1, IMC =< IMCRange2
+    ), ListPinguins),
+    random_permutation(ListPinguins, RandomList),
+    group(RandomList, N, Pinguins, Sexe).
 
 
-display_groups([]).
-display_groups([Group | RestGroups]) :-
+% Création des groupes par IMC
+create_all_groups_imc(AllGroups, N) :-
+    create_groups_by_imc(N, female, AllFemaleGroups),
+    create_groups_by_imc(N, male, AllMaleGroups),
+    append(AllFemaleGroups, AllMaleGroups, AllGroups).
+
+create_groups_by_imc(N, Sexe, AllGroups) :-
+    findall(Group, group_pinguin_imc(Group, N, Sexe, 0, 1), LowImcGroups),
+    findall(Group, group_pinguin_imc(Group, N, Sexe, 1, 1.5), MediumImcGroups),
+    findall(Group, group_pinguin_imc(Group, N, Sexe, 1.5, 999), HighImcGroups),
+    append([LowImcGroups, MediumImcGroups, HighImcGroups], AllGroups).
+
+
+% Affichage des groupes
+display_groups_imc([]).
+display_groups_imc([Groups | RestGroups]) :-
+    nl,
+    display_groups_by_imc(Groups),
+    display_groups_imc(RestGroups).
+
+display_groups_by_imc([]).
+display_groups_by_imc([Group | RestGroups]) :-
     nl,
     write('Group: '), write(Group), nl,
     display_pinguins(Group),
-    display_groups(RestGroups).
+    display_groups_by_imc(RestGroups).
 
+% Affichage des pingouins
 display_pinguins([]).
 display_pinguins([P | RestPinguins]) :-
     pinguin(P, Nom, Taille, Poids, Sexe, Niveau, Position),
+    calculate_imc(Poids, Taille, IMC),
     write('  Pinguin '), write(P), write(': '),
     write('Nom: '), write(Nom), write(', '),
     write('Taille: '), write(Taille), write(', '),
     write('Poids: '), write(Poids), write(', '),
     write('Sexe: '), write(Sexe), write(', '),
     write('Niveau: '), write(Niveau), write(', '),
-    write('Position: '), write(Position), write(', '),
-    calculate_imc(Poids, Taille, IMC) , write('IMC: ') , write(IMC), nl,
+    write('Position: '), write(Position), nl,
+    write('IMC: '), write(IMC), nl,
     display_pinguins(RestPinguins).
 
-?- create_all_groups(AllGroups, 6), display_groups(AllGroups).
+% Utilisation du nouveau code
+?- create_all_groups_imc(AllGroups, 5), display_groups_imc(AllGroups).
