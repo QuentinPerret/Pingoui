d2f5a3ff1166db6786b9ac3dea12e26bc0a7b9eb
QuentinPerret <quentinprrt33@gmail.com>
2023-11-29 15:13:14 +0100

    try sort by bmi

--- repartition2.pl
+++ repartition2.pl
@@ -7,11 +7,38 @@
 calculate_imc(Weigh, Height, IMC) :-
     IMC is Weigh / (Height * Height).
 
-group_pinguin(Pinguins,N,Sexe) :- 
-    findall(P, pinguin(P,_,_,_,Sexe,_,_), ListPinguins),
-    random_permutation(ListPinguins, RandomList),
-    group(RandomList,N,Pinguins,Sexe).
+sexe_is(P ,Sexe) :- pinguin(P,_,_,_,Sexe,_,_). 
 
+group_pinguin(Pinguins, N, Sexe) :-
+    findall(P, pinguin(P, _, _, _, Sexe, _, _), ListPinguins),
+    random_permutation(ListPinguins, RandomList),
+    sort_by_imc(RandomList, SortedList, Sexe),
+    display_pinguins(SortedList),nl,
+    group(SortedList, N, Pinguins, Sexe).
+
+% Sort by BMI in descending order
+sort_by_imc(List, SortedList, Sexe) :-
+    include([P] >> (sexe_is(P,Sexe)), List, FiltreredList),
+    categorize_by_imc(FiltreredList, SortedList).
+
+
+% Categorize pinguins by BMI
+categorize_by_imc(Pinguins, SortedList) :-
+    categorize_by_imc_helper(Pinguins, [], [], [], SortedList).
+
+categorize_by_imc_helper([], L1, L2, L3, SortedList) :-
+    append(L1, L2, TempList),
+    append(TempList, L3, SortedList).
+
+categorize_by_imc_helper([P | Rest], L1, L2, L3, SortedList) :-
+    pinguin(P, _, Taille, Poids, _, _, _),
+    calculate_imc(Poids,Taille,IMC),
+    (IMC < 1 -> categorize_by_imc_helper(Rest, [P | L1], L2, L3, SortedList);
+     IMC =< 1.5 -> categorize_by_imc_helper(Rest, L1, [P | L2], L3, SortedList);
+     categorize_by_imc_helper(Rest, L1, L2, [P | L3], SortedList)).
+
+
+% Group pinguins
 group([],_,[],_).
 group(List , N,  [Group | Groups], Sexe) :-
     length(Group , N),
@@ -48,4 +75,5 @@ display_pinguins([P | RestPinguins]) :-
     calculate_imc(Poids, Taille, IMC) , write('IMC: ') , write(IMC), nl,
     display_pinguins(RestPinguins).
 
-?- create_all_groups(AllGroups, 6), display_groups(AllGroups).
+?- create_all_groups(AllGroups, 5).
+% , display_groups(AllGroups).
