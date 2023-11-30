4f91b7b5523611dc266d72a6a0d34e963380dc8e
QuentinPerret <quentinprrt33@gmail.com>
2023-11-30 15:55:06 +0100

    créer circuit sans remise

--- pistes.pl
+++ pistes.pl
@@ -1,4 +1,29 @@
-piste(0,'Mario Circuit',0).
-piste(1,'Toads Turnpike',1).
-piste(2,'Delfino Pier',2).
-piste(3,'Baby Park',2).
+piste(0,'Coconut Mall',0).
+piste(1,'Mushroom Bridge',1).
+piste(2,'Luigi Raceway',2).
+piste(3,'Toads Turnpike',1).
+piste(4,'Mushroom Gorge',1).
+piste(5,'DKs Jungle Parkway',1).
+piste(6,'Bowsers Castle',3).
+piste(7,'Maple Treeway',2).
+piste(8,'Shy Guy Beach',3).
+piste(9,'Wario Stadium',3).
+piste(10,'Donut Plains',2).
+piste(11,'Sherbet Land',3).
+piste(12,'Koopa Troopa Beach',2).
+piste(13,'Baby Park',1).
+piste(14,'Dry Dry Desert',2).
+piste(15,'Peach Beach',3).
+piste(16,'Mario Circuit',2).
+piste(17,'Waluigi Stadium',2).
+piste(18,'Grumble Volcano',1).
+piste(19,'Yoshi Valley',2).
+piste(20,'Airship Fortress',1).
+piste(21,'Rainbow Road',2).
+piste(22,'Tick-Tock Clock',1).
+piste(23,'Delfino Pier',2).
+piste(24,'Moonview Highway',1).
+piste(25,'Bowsers Castle',2).
+piste(26,'Wario Stadium',2).
+piste(27,'Luigi Raceway',2).
+piste(28,'Peach Beach',2).
